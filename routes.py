b2617c7601fec7a827fef6d36a8fbf4ab48b313f
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2026-04-19 19:44:11 +0200

    Add PUT and DELETE methods

--- routes.py
+++ routes.py
@@ -6,12 +6,12 @@ from extensions import db
 bp = Blueprint('api', __name__)
 
 @bp.route('/distros', methods=['GET'])
-def det_distros():
+def get_distros():
     distros = Distro.query.all()
     return jsonify([d.to_dict() for  d in distros])
 
 @bp.route('/distros/<int:id>', methods=['GET'])
-def method_name(id):
+def get_single(id):
     distro = Distro.query.get_or_404(id)
     return jsonify(distro.to_dict())
 
@@ -33,3 +33,28 @@ def create_distro():
     db.session.commit()
 
     return jsonify(new_distro.to_dict()), 201
+
+@bp.route('/distros/<int:id>', methods=['PUT'])
+def update_distro(id):
+    distro = Distro.query.get_or_404(id)
+    data = request.json
+
+    distro.name = data.get('name', distro.name)
+    distro.based_on = data.get('based_on', distro.based_on)
+    distro.release_date = data.get('release_date', distro.release_date)
+    distro.version = data.get('version', distro.version)
+    distro.developers = data.get('developers', distro.developers)
+
+    db.session.commit()
+
+    return jsonify(distro.to_dict())
+
+
+
+@bp.route('/distros/<int:id>', methods=['DELETE'])
+def delete_distro(id):
+    distro = Distro.query.get_or_404(id)
+    db.session.delete(distro)
+    db.session.commit()
+
+    return jsonify({"message": "Distribution deleted successfully"}) 
